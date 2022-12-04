547193cefaa3e8fbd3478303635b3b676471e7de
Peng-Yu Chen <walkccray@gmail.com>
2022-12-04 00:41:06 -0500

    7.4 Hooking Up the Interpreter

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,15 @@
 package com.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
@@ -96,6 +105,21 @@ public class Interpreter implements Expr.Visitor<Object> {
     if (a == null)
       return false;
     return a.equals(b);
+  }
+
+  private String stringify(Object object) {
+    if (object == null)
+      return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
   }
 
   private Object evaluate(Expr expr) {
