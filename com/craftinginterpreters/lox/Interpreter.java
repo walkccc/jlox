928c073286b769170f99dddaec8503ca9217001c
Peng-Yu Chen <walkccray@gmail.com>
2022-12-03 22:26:43 -0500

    7.2.2 Evaluating parentheses

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -2,7 +2,16 @@ package com.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
   @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
+  }
+
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
   }
 }
