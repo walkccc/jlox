adb56acca0a26f8d1e5227878b5f464b5d8cdca0
Peng-Yu Chen <walkccray@gmail.com>
2022-12-03 22:58:31 -0500

    7.2.5 Evaluating binary operators

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -2,6 +2,35 @@ package com.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
   @Override
+  public Object visitBinaryExpr(Expr.Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case GREATER: return (double) left > (double) right;
+      case GREATER_EQUAL: return (double) left >= (double) right;
+      case LESS: return (double) left < (double) right;
+      case LESS_EQUAL: return (double) left <= (double) right;
+      case MINUS: return (double) left - (double) right;
+      case PLUS:
+        if (left instanceof Double && right instanceof Double) {
+          return (double) left + (double) right;
+        }
+        if (left instanceof String && right instanceof String) {
+          return (String) left + (String) right;
+        }
+        break;
+      case SLASH: return (double) left / (double) right;
+      case STAR: return (double) left * (double) right;
+      case BANG_EQUAL: return !isEqual(left, right);
+      case EQUAL_EQUAL: return isEqual(left, right);
+      default: return null;
+    }
+
+    return null;
+  }
+
+  @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
   }
@@ -30,6 +59,16 @@ public class Interpreter implements Expr.Visitor<Object> {
     if (object instanceof Boolean)
       return (boolean) object;
     return true;
+  }
+
+  // You can't ask Lox if 3 is less than "three", but you can ask if it's equal
+  // to it.
+  private boolean isEqual(Object a, Object b) {
+    if (a == null && b == null)
+      return true;
+    if (a == null)
+      return false;
+    return a.equals(b);
   }
 
   private Object evaluate(Expr expr) {
