ac7cbe73eca45898f43f42a8583e51e60bf495ef
Peng-Yu Chen <walkccray@gmail.com>
2022-12-03 22:23:49 -0500

    7.2.1 Evaluating literals

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,3 +1,8 @@
 package com.craftinginterpreters.lox;
 
-public class Interpreter implements Expr.Visitor<Object> {}
+public class Interpreter implements Expr.Visitor<Object> {
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+}
