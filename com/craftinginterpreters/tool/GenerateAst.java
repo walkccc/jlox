ca9eda71833e99f115bcfd191dc8864223d9c695
Peng-Yu Chen <walkccray@gmail.com>
2022-12-04 17:19:18 -0500

    8.2 Global Variables - 8.2.1 Variable syntax

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -16,11 +16,13 @@ public class GenerateAst {
               Arrays.asList("Binary   : Expr left, Token operator, Expr right",
                             "Grouping : Expr expression",
                             "Literal  : Object value",
-                            "Unary    : Token operator, Expr right"));
+                            "Unary    : Token operator, Expr right",
+                            "Variable : Token name"));
 
     defineAst(outputDir, "Stmt",
               Arrays.asList("Expression : Expr expression",
-                            "Print      : Expr expression"));
+                            "Print      : Expr expression",
+                            "Var        : Token name, Expr initializer"));
   }
 
   private static void defineAst(String outputDir, String baseName,
