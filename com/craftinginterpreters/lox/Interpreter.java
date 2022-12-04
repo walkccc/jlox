5202dc680bcd79bed3789ad72c85858c027461c4
Peng-Yu Chen <walkccray@gmail.com>
2022-12-04 00:38:42 -0500

    7.3 Runtime Errors - 7.3.1 Detecting runtime errors

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -7,11 +7,21 @@ public class Interpreter implements Expr.Visitor<Object> {
     Object right = evaluate(expr.right);
 
     switch (expr.operator.type) {
-      case GREATER: return (double) left > (double) right;
-      case GREATER_EQUAL: return (double) left >= (double) right;
-      case LESS: return (double) left < (double) right;
-      case LESS_EQUAL: return (double) left <= (double) right;
-      case MINUS: return (double) left - (double) right;
+      case GREATER:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left > (double) right;
+      case GREATER_EQUAL:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left >= (double) right;
+      case LESS:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left < (double) right;
+      case LESS_EQUAL:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left <= (double) right;
+      case MINUS:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left - (double) right;
       case PLUS:
         if (left instanceof Double && right instanceof Double) {
           return (double) left + (double) right;
@@ -19,15 +29,18 @@ public class Interpreter implements Expr.Visitor<Object> {
         if (left instanceof String && right instanceof String) {
           return (String) left + (String) right;
         }
-        break;
-      case SLASH: return (double) left / (double) right;
-      case STAR: return (double) left * (double) right;
+        throw new RuntimeError(expr.operator,
+                               "Operands must be two numbers or two strings.");
+      case SLASH:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left / (double) right;
+      case STAR:
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left * (double) right;
       case BANG_EQUAL: return !isEqual(left, right);
       case EQUAL_EQUAL: return isEqual(left, right);
       default: return null;
     }
-
-    return null;
   }
 
   @Override
@@ -46,9 +59,23 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     switch (expr.operator.type) {
       case BANG: return !isTruthy(right);
-      case MINUS: return -(double) right;
+      case MINUS:
+        checkNumberOperand(expr.operator, right);
+        return -(double) right;
       default: return null;
     }
+  }
+
+  private void checkNumberOperand(Token operator, Object operand) {
+    if (operand instanceof Double)
+      return;
+    throw new RuntimeError(operator, "Operand must be a number.");
+  }
+
+  private void checkNumberOperands(Token operator, Object left, Object right) {
+    if (left instanceof Double && right instanceof Double)
+      return;
+    throw new RuntimeError(operator, "Operands must be numbers.");
   }
 
   // Lox follows Ruby's simple rule: false and nil are falsey, and everything
