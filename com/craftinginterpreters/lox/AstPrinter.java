83d329e777aa72068e604e1e6e681979af1686ca
Peng-Yu Chen <walkccray@gmail.com>
2022-12-08 21:49:16 -0500

    add: visitAssignExpr and visitVariableExpr in AstPrinter

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -6,6 +6,11 @@ class AstPrinter implements Expr.Visitor<String> {
   }
 
   @Override
+  public String visitAssignExpr(Expr.Assign expr) {
+    return parenthesize("= " + expr.name + " " + expr.value);
+  }
+
+  @Override
   public String visitBinaryExpr(Expr.Binary expr) {
     return parenthesize(expr.operator.lexeme, expr.left, expr.right);
   }
@@ -25,6 +30,11 @@ class AstPrinter implements Expr.Visitor<String> {
   @Override
   public String visitUnaryExpr(Expr.Unary expr) {
     return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  @Override
+  public String visitVariableExpr(Expr.Variable expr) {
+    return parenthesize("read", expr);
   }
 
   private String parenthesize(String name, Expr... exprs) {
