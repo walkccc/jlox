a52364227a1ab14f931784a8495bbdc2183cdf10
Peng-Yu Chen <walkccray@gmail.com>
2022-12-09 20:40:14 -0500

    10.1.2 Interpreting function calls

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -77,6 +78,17 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
       case EQUAL_EQUAL: return isEqual(left, right);
       default: return null;
     }
+  }
+
+  @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+    LoxCallable function = (LoxCallable) callee;
+    return function.call(this, arguments);
   }
 
   @Override
